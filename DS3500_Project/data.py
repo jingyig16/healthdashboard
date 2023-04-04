14677d3b9906e03c68cbc5d85fded49e39412160
jingyig16 <jingyig616@gmail.com>
2023-04-04 15:24:13 -0400

    changes + data

--- DS3500_Project/data.py
+++
@@ -1,8 +0,0 @@
-valid_variables = {
-    'D': ['TotalSteps', 'TotalDistance', 'TrackerDistance', 'LoggedActivitiesDistance',
-          'VeryActiveDistance', 'ModeratelyActiveDistance', 'LightActiveDistance',
-          'SedentaryActiveDistance', 'VeryActiveMinutes', 'FairlyActiveMinutes',
-          'LightlyActiveMinutes', 'SedentaryMinutes', 'Calories'],
-    'H': ['Calories', 'TotalIntensity', 'StepTotal'],
-    'M': ['Calories', 'Intensity', 'Steps']
-}
---
+++ data.py
@@ -0,0 +1,8 @@
+valid_variables = {
+    'D': ['TotalSteps', 'TotalDistance', 'TrackerDistance', 'LoggedActivitiesDistance',
+          'VeryActiveDistance', 'ModeratelyActiveDistance', 'LightActiveDistance',
+          'SedentaryActiveDistance', 'VeryActiveMinutes', 'FairlyActiveMinutes',
+          'LightlyActiveMinutes', 'SedentaryMinutes', 'Calories'],
+    'H': ['Calories', 'TotalIntensity', 'StepTotal'],
+    'M': ['Calories', 'Intensity', 'Steps']
+}
