e0c8c17cf2b7605fe9371feeafd5a2f133ff2997
Marco6666662 <1419699960@qq.com>
2023-04-05 19:25:23 -0400

    update correlation webpage

--- main.py
+++ main.py
@@ -9,22 +9,23 @@ from dash.exceptions import PreventUpdate
 
 from home import homepage
 from time_series import create_time_period_dropdown, category_dropdown_menu, create_time_series, time_series_page
+from correlation import create_time_period_dropdown, category_dropdown_menu1, category_dropdown_menu2, show_correlation, correlation_page
 from data import valid_variables
 
 
 # Function to read the data
 def read_data():
-    daily_activity = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/dailyActivity_merged.csv")
-    daily_sleep = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/sleepDay_merged.csv")
-    daily_weight = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/weightLogInfo_merged.csv")
-    hourly_calories = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/hourlyCalories_merged.csv")
-    hourly_intensities = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/hourlyIntensities_merged.csv")
-    hourly_steps = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/hourlySteps_merged.csv")
-    minute_calories_narrow = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/minuteCaloriesNarrow_merged.csv")
-    minute_intensities_narrow = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/minuteIntensitiesNarrow_merged.csv")
-    minute_steps_narrow = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/minuteStepsNarrow_merged.csv")
-    minute_sleep = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/minuteSleep_merged.csv")
-    second_heartrate = pd.read_csv("/Users/jaigollapudi/Downloads/Fitabase Data 4.12.16-5.12.16/heartrate_seconds_merged.csv")
+    daily_activity = pd.read_csv("data/dailyActivity_merged.csv")
+    daily_sleep = pd.read_csv("data/sleepDay_merged.csv")
+    daily_weight = pd.read_csv("data/weightLogInfo_merged.csv")
+    hourly_calories = pd.read_csv("data/hourlyCalories_merged.csv")
+    hourly_intensities = pd.read_csv("data/hourlyIntensities_merged.csv")
+    hourly_steps = pd.read_csv("data/hourlySteps_merged.csv")
+    minute_calories_narrow = pd.read_csv("data/minuteCaloriesNarrow_merged.csv")
+    minute_intensities_narrow = pd.read_csv("data/minuteIntensitiesNarrow_merged.csv")
+    minute_steps_narrow = pd.read_csv("data/minuteStepsNarrow_merged.csv")
+    minute_sleep = pd.read_csv("data/minuteSleep_merged.csv")
+    second_heartrate = pd.read_csv("data/heartrate_seconds_merged.csv")
 
     # Renaming column names for consistency over dataframes
     daily_activity = daily_activity.rename(columns={'ActivityDate': 'ActivityDay'})
@@ -111,6 +112,8 @@ def display_page(pathname):
     elif pathname == '/time-series':
         return time_series_page()
         # [Add more elif conditions for other pages here]
+    elif pathname == '/correlation':
+        return correlation_page()
     else:
         return homepage()
 
@@ -149,6 +152,46 @@ def update_time_series(variable, time_period, user_id):
 def update_category_dropdown(time_period):
     options = [{'label': v, 'value': v} for v in valid_variables[time_period]]
     return options
+
+###########################################################################################
+# Correlation Page
+# Callback to update the time series chart
+@app.callback(Output('correlation-chart', 'figure'),
+              Input('variable-dropdown1', 'value'),
+              Input('variable-dropdown2', 'value'),
+              Input('time-period-dropdown', 'value'),
+              Input('user-id', 'value'))
+def update_correlation(attr_1, attr_2, time_period, user_id):
+    if user_id is None:
+        return {}
+    # Update the options in the category_dropdown_menu
+    if time_period == 'D':
+        options = [{'label': v, 'value': v} for v in valid_variables['D']]
+    elif time_period == 'H':
+        options = [{'label': v, 'value': v} for v in valid_variables['H']]
+    elif time_period == 'M':
+        options = [{'label': v, 'value': v} for v in valid_variables['M']]
+    elif time_period =='S':
+        options = [{'label': v, 'value': v} for v in valid_variables['S']]
+    else:
+        options = []
+    category_dropdown_menu.options = options
+
+    # Check if the selected variable is valid for the selected time period
+    for attr in [attr_1, attr_2]:
+        if attr not in valid_variables[time_period]:
+            attr = valid_variables[time_period][0]
+
+    return show_correlation(attr_1, attr_2, time_period, user_id, data)
+
+
+# Callback to update category dropdown
+@app.callback(Output('variable-dropdown1', 'options'),
+              Output('variable-dropdown2', 'options'),
+              Input('time-period-dropdown', 'value'))
+def update_category_dropdown(time_period):
+    options = [{'label': v, 'value': v} for v in valid_variables[time_period]]
+    return options, options
 
 
 # Update the app's config to suppress_callback_exceptions
